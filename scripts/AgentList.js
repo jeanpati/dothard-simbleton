9ae0ed8a2263a3cf40bafa80f1268be8cfd619f1
Jean Patimeteeporn <jpatimet@gmail.com>
2024-12-07 00:54:54 -0600

    added an event listener to search businesses based on company name

--- scripts/AgentList.js
+++ scripts/AgentList.js
@@ -2,10 +2,8 @@ import { getBusinesses } from "./database.js";
 
 export const AgentList = () => {
   const allBusinesses = getBusinesses();
-  let agentHTML = "";
 
   const agentsArray = allBusinesses.map((business) => {
-    console.log(business.purchasingAgent.nameFirst);
     return `<section>
       <h3>
         ${business.purchasingAgent.nameFirst} ${business.purchasingAgent.nameLast}
@@ -13,8 +11,6 @@ export const AgentList = () => {
       <div>${business.companyName}  ${business.phoneWork}</div>
     </section>`;
   });
-
-  console.log(agentsArray);
 
   const joinedAgentArray = agentsArray.join("");
   return joinedAgentArray;
--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -15,3 +15,43 @@ export const BusinessList = () => {
   });
   return businessHTML;
 };
+
+document
+  .querySelector("#content")
+  .addEventListener("keypress", (keyPressEvent) => {
+    const companySearchResultArticle =
+      document.querySelector(".foundBusinesses");
+
+    if (keyPressEvent.charCode === 13) {
+      /*
+                        When the user presses 'Enter', find the matching business.
+
+
+                        You can use the `.includes()` string method to
+                        see if a smaller string is part of a larger string.
+
+                        Example:
+                            if (business.companyName.includes(keyPressEvent.target.value)) {
+
+                            }
+                    */
+
+      const allBusinesses = getBusinesses();
+      const foundBusiness = allBusinesses.find(
+        (business) =>
+          business.companyName
+            .toLocaleLowerCase()
+            .includes(keyPressEvent.target.value.toLocaleLowerCase()) ||
+          keyPressEvent.target.value ===
+            business.companyName.toLocaleLowerCase()
+      );
+
+      companySearchResultArticle.innerHTML = `<section class="business">
+            <h4>${foundBusiness.companyName}</h4>
+
+            <div class="business__address">${foundBusiness.addressFullStreet}</div>
+            <div class="business__address">${foundBusiness.addressCity}, ${foundBusiness.addressStateCode} ${foundBusiness.addressZipCode}</div>
+        </section>
+        `;
+    }
+  });
